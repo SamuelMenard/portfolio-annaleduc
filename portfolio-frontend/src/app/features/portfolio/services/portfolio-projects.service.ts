97c5c11385cc36d97f20c576d00b7bc1a37a88ab
Samuel Menard <samuel.menard@videotron.ca>
2023-03-17 17:50:46 -0400

    Portfolio started

--- portfolio-frontend/src/app/features/portfolio/services/portfolio-projects.service.ts
+++ portfolio-frontend/src/app/features/portfolio/services/portfolio-projects.service.ts
@@ -1,25 +1,50 @@
-import { Injectable } from '@angular/core';
-import { Project } from '../models/project.model';
+import { Injectable } from "@angular/core";
+import { Project } from "../models/project.model";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class PortfolioProjectsService {
-  private projects: Project[] = [
-    { id: 'projet1', title: 'Projet 1', description: 'Description 1', coverImageUrl: '' },
-    { id: 'projet2', title: 'Projet 2', description: 'Description 2', coverImageUrl: '' },
-    { id: 'projet3', title: 'Projet 3', description: 'Description 3', coverImageUrl: '' },
-    { id: 'projet4', title: 'Projet 4', description: 'Description 4', coverImageUrl: '' },
-    { id: 'projet5', title: 'Projet 5', description: 'Description 5', coverImageUrl: '' },
-  ];
+    private projects: Project[] = [
+        {
+            id: "projet1",
+            title: "Projet 1",
+            description: "Description 1",
+            coverImageUrl: "../../../../../assets/projects/projet-6/vue-3d-japandi.webp"
+        },
+        {
+            id: "projet2",
+            title: "Projet 2",
+            description: "Description 2",
+            coverImageUrl: "../../../../../assets/projects/projet-6/vue-3d-japandi.webp"
+        },
+        {
+            id: "projet3",
+            title: "Projet 3",
+            description: "Description 3",
+            coverImageUrl: "../../../../../assets/projects/projet-6/vue-3d-japandi.webp"
+        },
+        {
+            id: "projet4",
+            title: "Projet 4",
+            description: "Description 4",
+            coverImageUrl: "../../../../../assets/projects/projet-6/vue-3d-japandi.webp"
+        },
+        {
+            id: "projet5",
+            title: "Projet 5",
+            description: "Description 5",
+            coverImageUrl: "../../../../../assets/projects/projet-6/vue-3d-japandi.webp"
+        }
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  public getAllProjects(): Project[] {
-    return this.projects;
-  }
+    public getAllProjects(): Project[] {
+        return this.projects;
+    }
 
-  public getProjectById(id: string): Project | undefined {
-    return this.projects.find(project => project.id === id);
-  }
+    public getProjectById(id: string): Project | undefined {
+        return this.projects.find((project) => project.id === id);
+    }
 }
